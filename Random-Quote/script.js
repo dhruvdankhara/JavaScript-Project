6d53fcd92055a7add4bef46c2363853541a1bfe5
dhruvdankhara <dhruvdankhara09@gmail.com>
2024-01-24 13:10:43 +0530

    add api for quote

---
+++ Random-Quote/script.js
@@ -0,0 +1,18 @@
+let btn = document.querySelector("#btn")
+let p = document.querySelector("p")
+
+async function getQuote(){
+    const url = "https://api.quotable.io/random";
+    let response = await fetch(url);
+    let data = await response.json();
+    p.innerText = data.content;
+    // console.log(data.content);
+}
+
+btn.addEventListener("click", getQuote);
+
+
+
+// fetch("https://api.quotable.io/random")
+// .then((response) => console.log(response))
+// .catch( error => console.error(error));

87ffdccb842307ec5ac042b18613681912b20abe
dhruvdankhara <dhruvdankhara09@gmail.com>
2024-01-24 15:18:23 +0530

    added copy to clipboard button

--- Random-Quote/script.js
+++ Random-Quote/script.js
@@ -1,11 +1,20 @@
-let btn = document.querySelector("#btn")
-let p = document.querySelector("p")
+let btn = document.querySelector("#btn");
+let copy = document.querySelector(".copy");
+let p = document.querySelector("p");
+
 
 async function getQuote(){
     const url = "https://api.quotable.io/random";
     let response = await fetch(url);
     let data = await response.json();
     p.innerText = `"${data.content}"`;
+
+    const copyToClipboard = (content) => {
+        return navigator.clipboard.writeText(content);
+    }
+    
+    copy.addEventListener("click", copyToClipboard(data.content));
+
 }
 
 btn.addEventListener("click", getQuote);

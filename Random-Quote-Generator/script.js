f0d5762e1885f5ea600b7f75f16f85ec87270fb7
dhruvdankhara <dhruvdankhara09@gmail.com>
2024-02-18 23:12:36 +0530

    add loader

--- Random-Quote-Generator/script.js
+++ Random-Quote-Generator/script.js
@@ -1,6 +1,7 @@
 let btn = document.querySelector("#btn");
 let copy = document.querySelector(".copy");
-let p = document.querySelector("p");
+let p = document.querySelector(".quote");
+let loader = document.querySelector(".loader");
 
 function showToolTip() {
     let toolTip = document.querySelector(".tooltip-text");
@@ -11,10 +12,15 @@ function showToolTip() {
 }
 
 async function getQuote(){
+    p.style.display = "none";
+    loader.style.display = "block";
     const url = "https://api.quotable.io/random";
     let response = await fetch(url);
     let data = await response.json();
     p.innerText = `“${data.content}“`;
+
+    loader.style.display = "none"
+    p.style.display = "block";
 }
 
 copy.addEventListener("click", () => {
